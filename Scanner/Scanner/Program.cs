621eb825e4e3691230f37800d28ba00bc4f2f51e
Mykezero <as482jtm45@aol.com>
2014-06-08 18:36:39 -0400

    Added atom0s' files for memory reading and signature creation. Program.cs contains my first attempt at creating signatures and reading memory. The setup works, but one I restart versions of FFXIV or FFXI the memory addresses change. This could be a side effect of the signature being too far away from the address to be read. The offset is too large. The signature currently being read is the mp/cp of the main player in FFXIV.

--- Scanner/Scanner/Program.cs
+++ Scanner/Scanner/Program.cs
@@ -6,82 +6,83 @@ using System.Threading.Tasks;
 
 using System.Runtime.InteropServices;
 using System.Diagnostics;
-using Vivisection;
+using Clipper.Classes;
+using System.IO;
+using System.Threading;
 
 namespace Scanner
 {
     class Program
     {
-        public static List<Process> Processes 
-        { 
-            get 
-            { 
-                return Process.GetProcessesByName("pol").ToList(); 
-            } 
-        }
-
-        public static List<FFInstance> Instances 
-        {
-            get { return Processes.Select(x => new FFInstance(x)).Where(x => x.Valid).ToList(); }
-        }
+        static string FFXIV_GAME_NAME = "ffxiv";
+        static string FFXIV_PROCESS_NAME = "ffxiv.exe";
+        static string FFXI_GAME_NAME = "pol";
+        static string FFXI_PROCESS_NAME = "ffximain.dll";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Scanner now running....");
-            SigScanner Scanner = new SigScanner();
-            Scanner.Target = Instances[0];
 
-            if (Instances[0] == null) 
-            { 
-                Console.WriteLine("There are no instances currently loaded, shutting down.");
-                System.Environment.Exit(0);
-            }
+            // Get the process we are reading memory from.
+            Process process = Process.GetProcessesByName(FFXIV_GAME_NAME).FirstOrDefault();
 
-            var pdata = Instances[0].Instance.Target;
-
-            TARGETINFO findme = new TARGETINFO 
-             {
-                 CurrentID = pdata.ID,
-                 CurrentMask = pdata.Mask,
-                 CurrentSvrID = pdata.SubServerID,
-                 HPP = (byte)pdata.HPPCurrent,
-                 // IsLocked = pdata.IsLocked,
-                 // IsSub = pdata.IsSub,
-                 Name = pdata.Name,
-                 SubID = pdata.SubID,
-                 SubMask = pdata.SubMask,
-                 SubSrvID = pdata.SubServerID,
-             };
-
-            byte[] findsig = Scanner.StructToBytes(findme);
-
-            string findmeMask =
-                  "xxxx" // curid
-                + "xx" //curmask
-                + "xxxx" //cursvrid
-                + "xxxx"//hpp
-                + "????"//islocked
-                + "????" //issub
-                + "xxxx" //name
-                + "xxxx" //subid
-                + "xx" //submask
-                + "xxxx"; //subsrvid
-
-            IntPtr InventoryStart = Scanner.ScanModule("FFXiMain", findsig, findmeMask, 0);
-
-            Console.WriteLine("Signature : " + InventoryStart.ToString("X2"));
-
-            Console.ReadLine();
-        }
-
-        public static String CreateSig(int Number)
-        {
-            String mask = "";
-            for (int i = 0; i < Number; i++)
+            if (process == null)
             {
-                mask += "x";
+                Console.WriteLine("Error: process {0} not found.", FFXIV_GAME_NAME);
+                Console.ReadLine();
+                return;
             }
-            return mask;
+
+            // Get the main module that we are reading memory from.
+            var main = (from ProcessModule m in process.Modules
+                        where m.ModuleName.ToLower() == FFXIV_PROCESS_NAME
+                        select m).SingleOrDefault();
+
+
+            if (main == null)
+            {
+                Console.WriteLine("Error: main module {0} not found.", FFXIV_PROCESS_NAME);
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(main.ModuleName);
+
+            // Create our signature scanner for our process and module.
+            SigScan sigScan = new SigScan(
+                process, main.BaseAddress, main.ModuleMemorySize
+                );
+
+            // Signature for CP / MP
+            // Set the pattern mask and offset.           
+            var pattern = "8908C6431D018B462C8B4E288945E4894DE085C074333BC8";
+            var mask = "xxxxxxxxxxxxxxxxxxxxxxxx";
+            int offset = 0x135D4AA6;
+
+            //Signature and Mask for Aggroed creatures
+            //Used to access top three aggroing creatures
+            //8901C6431D018B4774 
+            //xxxxxxxxx
+
+            // Get the pointer from the signature and print it out
+            var pointer = sigScan.FindPattern(Helpers.HexStringToArray(pattern), mask, offset);
+            Console.WriteLine("Pointer is {0}.", (pointer).ToString("X"));
+            Console.ReadLine();
+
+            while (true)
+            {
+                Console.Clear();
+                // Get the memory at the address of the pointer, store it into bytesRead, convert it to an int and
+                // then print it out.
+                var tempBuffer = new byte[4];
+                Memory.Peek(process, pointer, tempBuffer);
+                var value = BitConverter.ToInt32(tempBuffer, 0);
+                Console.WriteLine("The value is {0}.", value);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Press enter to quit...");
+            Console.ReadLine();
         }
     }
 }
