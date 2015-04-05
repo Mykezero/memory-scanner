6bcad2b28269ce9cb6978598d04af2f2bce75cc1
Mike <as482jtm45@aol.com>
2015-04-05 18:28:38 -0400

    Updated ScannerTest class with more comments explaining what we are trying to test.

--- Scanner/ZScanLibTests/ScannerTests.cs
+++ Scanner/ZScanLibTests/ScannerTests.cs
@@ -9,56 +9,34 @@ namespace ZScanLibTests
     [TestClass]
     public class ScannerTests
     {
-        private Process process;
-
-        public ScannerTests()
-        {
-            // Grab first notepad session. 
-            process = Process.GetProcessesByName("pol").FirstOrDefault();
-        }
-
+        /// <summary>
+        /// Tests the functionality of the pattern matcher. 
+        /// </summary>
         [TestMethod]
         public void TestPatternMatcher()
         {
+            // Create the matcher
             PatternMatcher matcher = new PatternMatcher();
 
+            // Create a new predictable pattern. 
             var pattern = string.Join("", Enumerable.Repeat("0", 10000));
+
+            // Create a mask that matches the new pattern. 
             var mask = string.Join("", Enumerable.Repeat("X", 5000));
+            
             var offset = 0;
-                
+
+            // Create the signature object. 
             var signature = new Signature(pattern, mask, offset);
 
+            // Create the memory buffer to scan a pattern for. 
             byte[] bytes = Enumerable.Repeat<byte>(0x00, 5000).ToArray();
 
+            // Get the starting address for the matched signature. 
             var index = matcher.FindMatch(signature, bytes);
 
+            // Test to see if the signature was found. 
             Assert.IsTrue(index == 0);
-        }
-
-        public void Test()
-        {
-            // Auto-Fail on process not found. 
-            Assert.IsNotNull(process);
-
-            // The signature data for locating our player's hp in Final Fantasy XI. 
-            var pattern = "8D048D00000000B9000000008D780400000000C70000000000F3A5";
-            var mask = "XXX????X????XX????XX?????XX";
-            var offset = 0;
-
-            // Create a scanner and search for our signature. If one is not found
-            // we fail this test. 
-            Signature signature = new Signature(pattern, mask, offset);
-            MemoryScanner scanner = new MemoryScanner(process);
-            IntPtr address = scanner.Scan(signature);
-            Assert.IsTrue(address != IntPtr.Zero);
-
-            // Get the value
-            byte[] buffer = new byte[sizeof(int)];
-            Memory.Peek(process, address, buffer);
-            var health = BitConverter.ToInt32(buffer, 0);
-
-            // Return true if the health is in its normal range. 
-            Assert.IsTrue(health >= 0 || health <= 100);
         }
     }
 }
