d727dfc4269b01ccfa684490cd4bba02b583f3e0
Mike <as482jtm45@aol.com>
2015-04-18 19:59:31 -0400

    Including new experimental project for locating signatures in FFXIV and updated gitignore file.

---
+++ Scanner/FFXIV/FFXIV.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZScanLib;
+
+namespace FFXIV
+{
+    public class FFXIV
+    {
+        /// <summary>
+        /// The id of the process
+        /// </summary>
+        private int pid;
+
+        /// <summary>
+        /// The process to read memory from. 
+        /// </summary>
+        private Process process;
+
+        private IMemoryScanner scanner;
+
+        /// <summary>
+        /// A collection of signatures by name. 
+        /// </summary>
+        public Dictionary<String, Signature> SignatureMap = 
+            new Dictionary<string, Signature>();
+
+        public FFXIV(int pid)
+        {
+            process = Process.GetProcessById(pid);
+            scanner = new MemoryScanner(process);
+        }
+
+        public class Player
+        {
+            public int HPP 
+            { 
+                get; 
+                set; 
+            }
+        }
+    }
+}
